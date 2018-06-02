78d23f4156f2b0e3cab22ed3640d5d719b0f51c6
Ben Shiundu <benshiundu@gmail.com>
2018-06-03 02:25:50 +0300

    index - still working on it

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry,  } from 'react-native';
+import IntroScreen from './main/screens/introScreen';
 import MainTabs from './main/routes';
 
 AppRegistry.registerComponent('nav_tutorial', () => MainTabs);
