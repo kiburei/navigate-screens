12753e4f4bb5838160d5218ec155df0554b3b4d4
Ben Shiundu <benshiundu@gmail.com>
2018-07-25 23:04:19 +0300

    icon path

--- main/screens/homeScreen/HomeScreen.js
+++ main/screens/homeScreen/HomeScreen.js
@@ -3,13 +3,13 @@ import { View, Text, Button, StyleSheet, TouchableOpacity, Image, BackHandler } 
 import GridView from 'react-native-super-grid';
 import styles from './HomeScreen.styles';
 
-var icon = [
-            {image:require('../../assets/icons/qr.png')},
-            {image:require('../../assets/icons/shop.png')},
-            {image:require('../../assets/icons/agent.png')},
-            {image:require('../../assets/icons/atm.png')},
-            {image:require('../../assets/icons/send.png')},
-            {image:require('../../assets/icons/request.png')},
+const icon = [
+            require('../../assets/icons/qr_code.png'),
+            require('../../assets/icons/shop_no.png'),
+            require('../../assets/icons/agent.png'),
+            require('../../assets/icons/atm.png'),
+            require('../../assets/icons/send_money.png'),
+            require('../../assets/icons/request.png'),
           ];
 
 export default class HomeScreen extends Component {
@@ -39,18 +39,15 @@ export default class HomeScreen extends Component {
             <Text style={{fontSize: 8}}>xxxx-2332</Text>
           </View>
           <GridView
-            itemDimension={130}
+            itemDimension={150}
             items={items}
-            renderItem={item => (
+            renderItem={(item, i) => (
               <TouchableOpacity
                 onPress={() => this.props.navigation.navigate(item.link)}
                 style={styles.actions}>
                 <View style={{flex: 1, flexDirection: 'row'}}>
                   <View style={{flex: 1, justifyContent: 'center'}}>
-                    <Image source = {item.i}
-                      style={styles.icons}
-                      />
-                    <Text>{item[0]}</Text>
+                    <Image source = {icon[item.i]} style={styles.icons} />
                   </View>
                   <View style={{flex: 2}}>
                     <Text style={styles.actionHeader}>{item.name}</Text>
@@ -68,10 +65,10 @@ export default class HomeScreen extends Component {
 }
 
 const items = [
-  { name: 'Pay using QR Code', info: 'Pay for goods and services by scanning a QR code', link: 'QrCode', i: 1 },
-  { name: 'Pay using Shop No.', info: 'Pay for goods & services by entering shop number', link: 'ShopNo', i: 2 },
-  { name: 'Withdraw from Agent', info: 'Withdraw some money from a Bongz Agent', link: 'AgentWithdraw', i: 3 },
-  { name: 'Withdraw from ATM', info: 'Withdraw money from ATM', link: 'AtmWithdraw', i: 4 },
-  { name: 'Send Money to Others', info: 'Send money instantly to other Bongz user', link: 'SendMoney', i: 5 },
-  { name: 'Request Payment', info: 'Send QR code or number & request payment', link: 'ReceiveMoney', i: 6 },
+  { name: 'Pay using QR Code', info: 'Pay for goods and services by scanning a QR code', link: 'QrCode', i: 0 },
+  { name: 'Pay using Shop No.', info: 'Pay for goods & services by entering shop number', link: 'ShopNo', i: 1 },
+  { name: 'Withdraw from Agent', info: 'Withdraw some money from a Bongz Agent', link: 'AgentWithdraw', i: 2 },
+  { name: 'Withdraw from ATM', info: 'Withdraw money from ATM', link: 'AtmWithdraw', i: 3 },
+  { name: 'Send Money to Others', info: 'Send money instantly to other Bongz user', link: 'SendMoney', i: 4 },
+  { name: 'Request Payment', info: 'Send QR code or number & request payment', link: 'ReceiveMoney', i: 5 },
 ];
